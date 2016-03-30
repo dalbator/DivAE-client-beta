5edf8e45927a03c4020d762ce0c2421440c361f5
Dominique Mayrand <dalbator@gmail.com>
2016-03-30 17:26:43 -0400

    PIFace/GPUI
    cleared up some confusion for handling PI face and GPIO

---
+++ python/piface_ctrl.py
@@ -0,0 +1,61 @@
+
+
+#sudo apt-get install python3-pifacedigital-emulator
+GOTPIFACE = True;
+try:
+    import pifacedigitalio
+except ImportError:
+    print("PIFACE module not found");
+    GOTPIFACE = False;
+
+class RPIFaceDigitalController:
+    p = None;
+    
+    def __init__(self):
+        self.start()
+
+    def getPortStatus(self, portid):
+        retval = False;
+        if(portid == 0 or portid == 1): # fail safe
+            retval = self.isOn(portid)
+        return retval
+            
+    def turnOnOutput(self, portid):
+        if(portid == 0 or portid == 1): # fail safe
+            self.open(portid)
+
+    def turnOffOutput(self, portid):
+        if(portid == 0 or portid == 1): # fail safe
+            self.close(portid)
+
+    def isOn(self, relay):
+        retval = False;
+        if(GOTPIFACE):
+            if(self.p != None):
+                intval = self.p.relays[relay].value;
+            if(intval == 1):
+                retval = True;		
+        return retval;
+
+    def open(self, relay):
+        if(self.p != None):
+            self.p.relays[relay].value = 1;
+
+    def close( self, relay ):
+        if(self.p != None):
+            self.p.relays[relay].value = 0; 
+
+    def turnalloff(self):
+        self.close(0)
+        self.close(1)
+
+    def start(self):
+        if(GOTPIFACE):
+            self.p = pifacedigitalio.PiFaceDigital()
+            self.turnalloff()
+
+    def stop(self):
+        if(self.p != None):
+            self.p.deinit_board();
+            self.p = None;
+        
--- python/rpictrl.py
+++ python/rpictrl.py
@@ -20,13 +20,7 @@ except ImportError:
     GOTGPIO = False;
     print("GPIO module note found");
     
-#sudo apt-get install python3-pifacedigital-emulator
-GOTPIFACE = True;
-try:
-    import pifacedigitalio
-except ImportError:
-    print("PIFACE module not found");
-    GOTPIFACE = False;
+
 
 import time
 
@@ -86,54 +80,4 @@ def setGPIOLow(pgpio):
         GPIO.setup(int(pgpio), GPIO.OUT)
         GPIO.output(int(pgpio), GPIO.LOW);
 
-class RPIFaceDigitalController:
-    p = None;
-    
-    def __init__(self):
-        self.start()
 
-    def getPortStatus(self, portid):
-        retval = False;
-        if(portid == 0 or portid == 1): # fail safe
-            retval = self.isOn(portid)
-        return retval
-            
-    def turnOnOutput(self, portid):
-        if(portid == 0 or portid == 1): # fail safe
-            self.open(portid)
-
-    def turnOffOutput(self, portid):
-        if(portid == 0 or portid == 1): # fail safe
-            self.close(portid)
-
-    def isOn(self, relay):
-        retval = False;
-        if(GOTPIFACE):
-            if(p != None):
-                intval = self.p.relays[relay].value;
-            if(intval == 1):
-                retval = True;		
-        return retval;
-
-    def open(self, relay):
-        if(self.p != None):
-            self.p.relays[relay].value = 1;
-
-    def close( self, relay ):
-        if(self.p != None):
-            self.p.relays[relay].value = 0; 
-
-    def turnalloff(self):
-        self.close(0)
-        self.close(1)
-
-    def start(self):
-        if(GOTPIFACE):
-            self.p = pifacedigitalio.PiFaceDigital()
-            self.turnalloff()
-
-    def stop(self):
-        if(self.p != None):
-            self.p.deinit_board();
-            self.p = None;
-        
